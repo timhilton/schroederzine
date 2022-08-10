cce52702018b267533ae8caa90294074b65e2866
Tim Hilton <tim@timhilton.net>
2022-08-09 23:49:01 -0700

    splash page for schroeder zine

---
+++ src/components/Spash.jsx
@@ -0,0 +1,15 @@
+const Splash = () => {
+    return (
+        <section className="splash">
+            <h1 className="title">
+                <span className="title-small">
+                    the
+                </span>
+                Schroeder Zine
+            </h1>
+            <p>Est. 1995</p>
+        </section>
+    )
+}
+
+export default Splash;
