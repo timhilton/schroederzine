dfa683c9b7ee265ca6bae3e95b37f3a37100a788
Tim Hilton <tim@timhilton.net>
2022-08-12 16:12:57 -0700

    feat(cms): contentful addition, styling updates, about page update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import './App.css';
 import Nav from './components/Nav';
 import {
@@ -12,19 +13,60 @@ import Home from './pages/Home';
 import Articles from './pages/Articles';
 import Footer from './components/Footer';
 
+const query = `
+{
+    aboutCollection {
+        items {
+            title,
+            description,
+            desc {
+                json
+            }
+        }
+    }
+}
+`
 
 function App() {
+    const [aboutCollection, setAboutCollection] = useState(null);
+
+    useEffect(() => {
+        window
+            .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_SPACE_ID}`, {
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                    Authorization: `Bearer ${process.env.REACT_APP_CDI}`
+                },
+                body: JSON.stringify({ query })
+            })
+            .then((response) => response.json())
+            .then(({ data, errors}) => {
+                if(errors) {
+                    console.error(errors)
+                }
+
+                setAboutCollection(data.aboutCollection.items[0]);
+            });
+    }, []);
+
+    if (!aboutCollection) {
+        return "loading..."
+    }
+
     return (
         <div className="App">
             <Router>
                 <Nav/>
+                <main id="main">
                 <Routes>
                     <Route exact path='/' element={<Home/>}/>
-                    <Route exact path='/about' element={<About/>}/>
+                    <Route exact path='/about' element={<About about={aboutCollection}/>}/>
                     <Route exact path='/music' element={<Music/>}/>
                     <Route exact path='/movies' element={<Movies/>}/>
                     <Route exact path='/articles' element={<Articles/>}/>
                 </Routes>
+                </main>
                 <Footer/>
             </Router>
         </div>
