de8cc7ce00dea38f0e8865541d6a592e3b3ae95f
Tim Hilton <tim@timhilton.net>
2022-08-13 15:19:21 -0700

    attempt to getStaticPaths

--- pages/articles/[id].jsx
+++ pages/articles/[id].jsx
@@ -7,12 +7,33 @@ const ArticlePage = ({article}) => {
     )
 }
 
-ArticlePage.getInitialProps = async (ctx) => {
-    const { id } = ctx.query;
-    const res = await ContentfulApi.getArticles();
-    const title = id.split('-').join(' ');
-    const item = res.filter((res) => res.title.toLowerCase() === title);
-    return { article: item[0] };
+export async function getStaticPaths() {
+    const articleList = await ContentfulApi.getArticles();
+    articleList.forEach((article) => {
+        article.title = article.title.toLowerCase().replace(' ', '-');
+    })
+
+    const paths = articleList.map((article) => ({
+        params: { id: article.title}
+    }))
+
+    return {
+        paths,
+        fallback: false
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const {id} = params;
+    const articleList = await ContentfulApi.getArticles();
+    const title = id.replace('-', ' ');
+    const item = articleList.filter((articleList) => articleList.title.toLowerCase() === title);
+
+    return {
+        props: {
+            article: item[0]
+        }
+    };
 }
 
 export default ArticlePage;
