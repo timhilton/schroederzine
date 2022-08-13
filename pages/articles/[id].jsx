cd55b79aab5d22bda1fdece137d99f3427ed1fb4
Tim Hilton <tim@timhilton.net>
2022-08-13 15:24:24 -0700

    minor code cleanup

--- pages/articles/[id].jsx
+++ pages/articles/[id].jsx
@@ -14,7 +14,7 @@ export async function getStaticPaths() {
     })
 
     const paths = articleList.map((article) => ({
-        params: { id: article.title}
+        params: { id: article.title }
     }))
 
     return {
