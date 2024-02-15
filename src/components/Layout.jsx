2e68beb55de3ad3d510febe69be0bcd4845118a1
Tim Hilton <tim@timhilton.net>
2024-02-15 11:11:35 -0800

    adds vercel analytics

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import Nav from "./Nav"
 import Footer from "./Footer"
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Layout({ children }) {
   return (
@@ -9,6 +10,7 @@ export default function Layout({ children }) {
         <main id="main">{children}</main>
         <Footer/>
         <SpeedInsights/>
+        <Analytics/>
     </>
   )
 }
